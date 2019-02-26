435a7a649ff5ee5b6c126a59170dccce76ecc4a6
Fabiano Dutra <fmbdutra@gmail.com>
2019-02-26 14:28:46 -0300

    add 3 fotos no html

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -4,4 +4,14 @@ angular.module('alurapic').controller('FotosController', function($scope) {
          titulo: 'Leão',
          url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
     };
+
+    $scope.foto2 = {
+        titulo: 'Leão2',
+        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
+
+    $scope.foto3 = {
+        titulo: 'Leão3',
+        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+    };
 });
