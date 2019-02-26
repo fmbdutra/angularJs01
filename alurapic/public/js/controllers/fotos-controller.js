b43009950ac7071ce6044fd768f9b5859e2995c0
Fabiano Dutra <fmbdutra@gmail.com>
2019-02-26 15:05:06 -0300

    add controller fotos pelo banco

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,17 +1,21 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
-    $scope.fotos =[
-        {
-            titulo: 'Leão',
-            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        },
-        {
-            titulo: 'Leão2',
-            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        },
-        {
-            titulo: 'Leão3',
-            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        }
-    ];
-});
+    $scope.fotos =[];
+
+/*     var promise = $http.get('/v1/fotos'); //$http.get nos retorna é uma 'promise'
+    promise.then(function(retorno){
+        $scope.fotos = retorno.data;
+    })
+    .catch(function(erro){
+        console.log(erro)
+    }); */
+
+    $http.get('/v1/fotos')
+        .success(function(retorno) {
+            console.log(retorno);
+            $scope.fotos = retorno; // não precisa fazer retorno.data
+        })
+        .error(function(erro) {
+            console.log(erro);
+        });         
+}); 
