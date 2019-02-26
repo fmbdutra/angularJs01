60d702407766308cd0c9343142db4c56f7330a21
Fabiano Dutra <fmbdutra@gmail.com>
2019-02-26 14:38:18 -0300

    add repeat angular

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,17 +1,17 @@
 angular.module('alurapic').controller('FotosController', function($scope) {
 
-    $scope.foto = {
-         titulo: 'Leão',
-         url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
-
-    $scope.foto2 = {
-        titulo: 'Leão2',
-        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
-
-    $scope.foto3 = {
-        titulo: 'Leão3',
-        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
+    $scope.fotos =[
+        {
+            titulo: 'Leão',
+            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+        },
+        {
+            titulo: 'Leão2',
+            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+        },
+        {
+            titulo: 'Leão3',
+            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+        }
+    ];
 });
