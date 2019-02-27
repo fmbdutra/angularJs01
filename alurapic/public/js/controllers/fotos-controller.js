5ff67f0a2418e100f5c4f212e1763df263741aab
Fabiano Dutra <fmbdutra@gmail.com>
2019-02-27 10:28:37 -0300

    add filtro dinamico

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -1,15 +1,18 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
     $scope.fotos =[];
+    $scope.filtro = '';
 
-/*     var promise = $http.get('/v1/fotos'); //$http.get nos retorna é uma 'promise'
+    /*  
+    var promise = $http.get('/v1/fotos'); //$http.get nos retorna é uma 'promise'
     promise.then(function(retorno){
         $scope.fotos = retorno.data;
     })
     .catch(function(erro){
         console.log(erro)
-    }); */
-
+    }); 
+    */
+    //Acima está certo, porém podemos usar assim com sucess e error:
     $http.get('/v1/fotos')
         .success(function(retorno) {
             console.log(retorno);
