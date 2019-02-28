659f2d9f55ce5b477558ab902748c82ad3e27cec
Fabiano Dutra <fmbdutra@gmail.com>
2019-02-28 16:18:53 -0300

    add mais validações

--- alurapic/public/js/controllers/foto-controller.js
+++ alurapic/public/js/controllers/foto-controller.js
@@ -1,9 +1,22 @@
 angular.module('alurapic')
-    .controller('FotoController', function($scope) {
+.controller('FotoController', function($scope, $http) {
 
-        $scope.foto = {};
-        $scope.submeter = function() {
-            console.log($scope.foto);
-        };
+    $scope.foto = {};
+    $scope.mensagem = '';
 
-    });
+    $scope.submeter = function() {
+
+        if ($scope.formulario.$valid) {
+
+            $http.post('/v1/fotos', $scope.foto)
+            .success(function() {
+                console.log('Foto adicionada com sucesso');
+                $scope.mensagem = 'Foto cadastrada com sucesso';
+            })
+            .error(function(erro) {
+                console.log('Não foi possível cadastra a foto');
+                $scope.mensagem = 'Não foi possível cadastrar a foto';
+            })
+        }
+    };
+});
